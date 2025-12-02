94beb3f8e74556fee5fe7347a5730434ee028600
Smr0119 <yangzhengzhang0119@gmail.com>
2025-12-01 20:09:42 -0500

    Added frontend links

---
+++ db/migrate/20251201230014_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :name
+      t.string :role, default: "member", null: false
+      t.boolean :active, default: true, null: false
+      t.string :otp_secret
+      t.boolean :otp_enabled, default: false, null: false
+      t.text :backup_code_digests
+      t.string :reset_digest
+      t.datetime :reset_sent_at
+      t.string :google_uid
+      t.text :google_token
+      t.text :google_refresh_token
+      t.datetime :google_token_expires_at
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
