bd291d24484741c9ed2e4987fc61bfcf9bd188ca
Antonio Baranes <157712299+AntonioBaranes@users.noreply.github.com>
2025-12-03 10:27:52 -0500

    Add comments table to database schema
    Added end cuz i can't read and because alan review good

--- db/schema.rb
+++ db/schema.rb
@@ -86,6 +86,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_02_015308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_teams_on_user_id"
+  end
+  
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
