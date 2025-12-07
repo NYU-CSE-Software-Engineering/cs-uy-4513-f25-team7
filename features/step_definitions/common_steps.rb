3f33e67fb26d84a1316c283a48b50e0c171f6e13
AmericanCelsius <davidrokicki1@gmail.com>
2025-12-06 21:34:27 -0500

    Fix social specs and forum registered-user step for Cucumber

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -8,3 +8,11 @@ end
 When('I press {string}') do |button|
   click_button button
 end
+
+Given('I am a registered user') do
+  @current_user ||= User.find_or_create_by!(email: "test@example.com") do |u|
+    u.password = "password123"
+    u.password_confirmation = "password123"
+  end
+  @user = @current_user
+end
