6f03bab6c062adf84c1c25ed7fefabcc0f1a9238
Smr0119 <yangzhengzhang0119@gmail.com>
2025-12-07 23:35:33 -0500

    CSS/JS Beautification Complete

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to a post' do
+      association = described_class.reflect_on_association(:post)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe 'validations' do
+    it 'requires a body' do
+      comment = Comment.new(body: nil)
+      comment.valid?
+      expect(comment.errors[:body]).to include("can't be blank")
+    end
+  end
 end
