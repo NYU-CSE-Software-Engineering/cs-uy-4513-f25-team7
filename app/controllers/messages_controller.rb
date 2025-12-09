a89ab8c39717bc7c91a6e981ceca737cf94dc6e9
Joe Leighthardt <joe@10-16-115-152.dynapool.wireless.nyu.edu>
2025-12-09 17:32:44 -0500

    message controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,56 @@
+class MessagesController < ApplicationController
+  before_action :ensure_authenticated
+  before_action :set_message, only: [:show]
+
+  def index
+    # Inbox: only messages where the current user is the recipient
+    @messages = current_user.received_messages.order(created_at: :desc)
+  end
+
+  def new
+    @message = Message.new
+    if params[:recipient_id].present?
+      @recipient = User.find_by(id: params[:recipient_id])
+      @message.recipient = @recipient if @recipient
+    end
+  end
+
+  def create
+    # Sender is always the current signed-in user
+    @message = current_user.sent_messages.build(message_params)
+
+    if @message.save
+      flash[:notice] = "Message sent."
+      redirect_to message_path(@message)
+    else
+      flash.now[:alert] = "Unable to send messages."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    # Only sender or recipient can view the messages
+    unless [@message.sender_id, @message.recipient_id].include?(current_user.id)
+      head :not_found and return
+    end
+
+    # Mark as read when the recipient views it
+    if @message.recipient_id == current_user.id && @message.read_at.nil?
+      @message.mark_read!
+    end
+  end
+
+  private
+
+  def set_message
+    # Look up only within messages involving the current user
+    @message = Message
+                 .where(sender_id: current_user.id)
+                 .or(Message.where(recipient_id: current_user.id))
+                 .find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:recipient_id, :subject, :body)
+  end
+end
