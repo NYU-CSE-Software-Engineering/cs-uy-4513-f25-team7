5a2a9f856a1fdbf314a44334db4a7459ef94dedc
Joe Leighthardt <joe@10-16-115-152.dynapool.wireless.nyu.edu>
2025-12-09 17:30:38 -0500

    Step definitions for DMs

---
+++ features/step_definitions/message_steps.rb
@@ -0,0 +1,23 @@
+# features/step_definitions/message_steps.rb
+# Step definitions for direct messaging features
+
+Given('the user {string} has sent me a message {string}') do |name, subject|
+  # Ensure we have a signed-in current user (reuses auth_steps)
+  step 'I am signed in'
+
+  sender = user_by_name(name) # helper defined in social_graph_notifications_steps.rb
+  Message.create!(
+    sender: sender,
+    recipient: @current_user,
+    subject: subject,
+    body: "Body for #{subject}"
+  )
+end
+
+When('I visit the messages inbox') do
+  visit messages_path
+end
+
+When('I fill in "Subject" with {string}') do |subject|
+  fill_in "Subject", with: subject
+end
