b7af7b65331d8963e9166b6bc58914a234dc9918
AmericanCelsius <davidrokicki1@gmail.com>
2025-12-08 23:35:36 -0500

    Add My Feed page and navigation

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,11 +1,25 @@
 class FeedController < ApplicationController
-  layout false  # avoid sprockets/layout asset writes during these tests
 
-  def show
+  def index
     @followed_names = FollowsController.followed_species
     posts = FakePostStore.all
 
-    followed_posts, other_posts = posts.partition { |p| @followed_names.include?(p.species) }
+    unless posts.any?
+      friend_ids = current_user&.followees&.pluck(:id) || []
+      friend_ids << current_user.id if current_user
+
+      scope = if friend_ids.any?
+                Post.where(user_id: friend_ids)
+              else
+                Post.none
+              end
+
+      posts = scope.includes(:user, :comments).order(created_at: :desc)
+    end
+
+    followed_posts, other_posts = posts.partition do |p|
+      p.respond_to?(:species) && @followed_names.include?(p.species)
+    end
     @ordered_posts = followed_posts + other_posts
   end
 end
